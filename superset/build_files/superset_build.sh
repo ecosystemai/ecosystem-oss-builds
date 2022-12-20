7d939d50af26e6b9ad013a96506065203b845c24
ecosystemai <jay@ecosystem.ai>
2022-12-20 18:32:46 +0200

    update

--- superset/build_files/superset_build.sh
+++ superset/build_files/superset_build.sh
@@ -1,1 +1,3 @@
-docker buildx build --platform linux/amd64,linux/arm64 -t ecosystemai/ecosystem-superset .
+# docker buildx build --platform linux/amd64,linux/arm64 -t ecosystemai/ecosystem-superset .
+docker build -t ecosystemai/ecosystem-superset .
+
