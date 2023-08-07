e091a3faab4d6720d191fcc703171a425c288709
jay <jay@ecosystem.ai>
2023-08-07 15:08:34 +0200

    updates

--- presto/03-run-container-local.sh
+++ presto/03-run-container-local.sh
@@ -1,3 +1,4 @@
 docker run --rm -it -p 8084:8084 \
    --net-alias ecosystem-worker-presto --network ecosystem --name ecosystem-worker-presto \
+   -v /Users/jayvanzyl/Documents/Projects/ecosystem-ai-next/ecosystem-server/dependencies/ecosystem-unix-starter/ecosystem-unix-starter/30-run-presto/etc:/app/etc \
    ecosystemai/ecosystem-worker-presto
--- presto/03-run.sh
+++ presto/03-run.sh
@@ -1,3 +1,4 @@
-docker run --rm -it -p 8084:8084 --net-alias ecosystem-worker-presto --network ecosystem \
+docker run --rm -it -p 8084:8084 \
+   --net-alias ecosystem-worker-presto --network ecosystem --name ecosystem-worker-presto \
    -v ~/ecosystem-unix-starter/04_config_ecosystem_presto/etc:/app/etc \
    ecosystemai/ecosystem-worker-presto
